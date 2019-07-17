dd16ebbfbbd59f07a246c02f250495a38bc4a2ff
Vincent Lecourt <lecourt.vincent@free.fr>
2019-07-17 10:30:32 +0200

    premier commitl

---
+++ Code/Back/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+/*
+* statut
+*/
+Route::get('/statut/{ip}', 'statutController@show')->name('statut.show');
+Route::get('/cmd/{ip}/{cmd}', 'statutController@cmd')->name('statut.cmd');
+//Route::get('/console', 'consoleController@show')->name('console.show');
